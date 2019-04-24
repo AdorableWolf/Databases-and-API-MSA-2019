cf1ec26feded897525f70dcdf7a7f7718e3a5d52
Sanket Patil <sanket1998@outlook.com>
2019-04-24 12:35:06 +1200

    Add the submisson files for Databases and API course for MSA 2019

---
+++ schoolSIMS/Model/nzmsasims.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace schoolSIMS.Model
+{
+    public partial class nzmsasims : DbContext
+    {
+        public nzmsasims()
+        {
+        }
+
+        public nzmsasims(DbContextOptions<nzmsasims> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tcp:nzmsasims.database.windows.net,1433;Initial Catalog=schoolSIMS;Persist Security Info=False;User ID=spt909;Password=Nz_msa1998; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.DegreeOfStudy).IsUnicode(false);
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+
+                entity.Property(e => e.StudentType).IsUnicode(false);
+            });
+        }
+    }
+}
